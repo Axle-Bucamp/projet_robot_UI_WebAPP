9af15371d19093859a4400feccf8b63665600987
Axle-Bucamp <47558509+Axle-Bucamp@users.noreply.github.com>
2019-03-14 13:29:32 +0100

    Add files via upload

--- robot_control_web/js/script.js
+++ robot_control_web/js/script.js
@@ -13,58 +13,47 @@ $('.menu-mobile .menu-m').click(function(){
 var jsonparse = JSON.parse(json);
 
 //generation du select
-option = document.createElement("option");
-option.text =  jsonparse.text;
-option.value = jsonparse.text;
-document.getElementById("activitySelector").add(option);
-$("#activitySelector").html('<option value='+""+' disable selected > choose your robots</option>');
 jQuery.each(jsonparse,function(key, value){
 	
 	jQuery.each(value,function(key2, value2){
 		if(key2 === "name"){
 			$("#activitySelector").append($('<option value='+'"' + key + '"'+' > ' + value2 + '</option>'));
-			
+
 		}
 	});	
 });
-
-$('select').formSelect('destroy');
+// init selection list
 $("#activitySelector").formSelect();
 var selected = jsonparse["1"];
+
 //lorsque l'on selectionne un robot
 var activities = document.getElementById("activitySelector");
 activities.addEventListener("change", function() {
 	var a = $(this).val();
-	jQuery.each(jsonparse,function(key, value){
+	robot_change(a);
+});
+// gere le changement de robot (genere le html necess et change l'element courant selectionné)
+function robot_change(a) {
+  jQuery.each(jsonparse,function(key, value){
 			if(key === ""+ a){
 				selected = jsonparse[key];
 				jQuery.each(value,function(key2, value2){
 					if(key2 === "text"){
 						$("#robots_infos").html("<p>" + value2 + "</p>");
-						$(".information p").html(value2);
+						$(".information p.rText").html(value2);
 					}
 					if(key2 === "img"){
 						$("#robots_img").html("<img src='img/" + value2 + "'/>");
+						$(".information #img").html("<img src='img/" + value2 + "'/>");
+					}
+					if(key2 === "name"){
+						$(".information h3.nRobot span").html( value2);
 					}
 			});	
 		}
 	});
-});
-
-
-
-/* si l actualisation ne suffit plus pour le changement d oriantation 
-var mql = window.matchMedia("(orientation: portrait)");
-mql.addListener(handleOrientationChange);
-handleOrientationChange(mql);
-function handleOrientationChange(mql) {
-  if (mql.matches) {
-    
-  } else {
-    
-    document.getElementsByClassName('capteurEtat').style.top = "90vh";
-    document.getElementsByClassName('capteurEtat').style.right = "15em";
-  }
-}*/
+}
+// premier appelle par défault
+robot_change("1");
 
 });
